21e9fcbbf103d839558db8ba276fdb0a36146f92
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-11-20 20:13:49 +1300

    Fix Active *

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -31,7 +31,7 @@
     </li>
 
     <!-- Nav Item - Transaksi -->
-    <li class="nav-item {{ request()->is('admin/transaction') ? 'active' : '' }}">
+    <li class="nav-item {{ request()->is('admin/transaction*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('transaction.index') }}">
             <i class="fas fa-fw fa-dollar-sign"></i>
             <span>Transaksi</span></a>
